3cfeb777237357ddffb8dff449c8558dc95dc2b2
邓学儒 <dengxueru@hengli.com>
2025-05-20 16:09:08 +0800

    feat: sqlite3数据库

--- database.ts
+++ database.ts
@@ -1,55 +1,28 @@
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
-const RealmDB = require("realm");
-import type { Configuration, ObjectSchema } from "realm";
+const sqlite3 = require("sqlite3").verbose();
 
 class Database {
   constructor() {
-    this.init();
+    const db = new sqlite3.Database("./sqlite3.node");
 
-    // const sqlite3 = require("sqlite3").verbose(); // 导入并启用详细模式
-  }
+    db.serialize(() => {
+      db.run("CREATE TABLE lorem (info TEXT)");
 
-  async init() {
-    class Car extends RealmDB.Object<Car> {
-      _id!: Realm.BSON.ObjectId;
-      make!: string;
-      model!: string;
-      miles?: number;
+      const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
 
-      static schema: ObjectSchema = {
-        name: "Car",
-        primaryKey: "_id",
-        properties: {
-          _id: { type: "objectId", default: () => new RealmDB.BSON.ObjectId() },
-          make: "string",
-          model: "string",
-          miles: "int?",
-        },
-      };
-    }
-
-    const config: Configuration = {
-      schema: [Car.schema],
-    };
-    const realm = await RealmDB.open(config);
-
-    realm.write(() => {
-      realm.create("Car", {
-        make: "benchi",
-        model: "glc",
-        miles: 10000,
-      });
+      db.each("SELECT rowid AS id, info FROM lorem", () => {});
     });
-    // const obj = realm.objects("Car");
-    // console.log(obj);
-    // const myTask = realm.objectForPrimaryKey(
-    //   "Car",
-    //   new Realm.BSON.ObjectId("682ace83df2699dd89312d58")
-    // );
-    // console.log(myTask);
+
+    db.close();
   }
+
+  async init() {}
 }
 
 export default Database;
