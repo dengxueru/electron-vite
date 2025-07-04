6ce692e5110c0e7ad96f3d76a36c188378a3503b
邓学儒 <dengxueru@hengli.com>
2025-07-04 13:58:16 +0800

    feat: 注销realm

--- realm/Car.ts
+++ realm/Car.ts
@@ -1,39 +1,39 @@
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-const RealmDB = require("realm");
+// import { createRequire } from "node:module";
+// const require = createRequire(import.meta.url);
+// const RealmDB = require("realm");
 
-import { Car } from "./type";
-import type { Realm, Configuration } from "realm";
-import { carItem } from "../types/electron";
+// import { Car } from "./type";
+// import type { Realm, Configuration } from "realm";
+// import { carItem } from "../types/electron";
 
-class CarDatabase {
-  realm: Realm;
+// class CarDatabase {
+//   realm: Realm;
 
-  constructor() {
-    this.init();
-  }
+//   constructor() {
+//     this.init();
+//   }
 
-  async init() {
-    const config: Configuration = {
-      schema: [Car.schema],
-    };
-    this.realm = await RealmDB.open(config);
-  }
+//   async init() {
+//     const config: Configuration = {
+//       schema: [Car.schema],
+//     };
+//     this.realm = await RealmDB.open(config);
+//   }
 
-  // 插入单条数据
-  insert(list: carItem[]) {
-    this.realm.write(() => {
-      list.forEach((item) => {
-        this.realm.create(Car.schema.name, item);
-      });
-    });
-  }
+//   // 插入单条数据
+//   insert(list: carItem[]) {
+//     this.realm.write(() => {
+//       list.forEach((item) => {
+//         this.realm.create(Car.schema.name, item);
+//       });
+//     });
+//   }
 
-  // 查询列表
-  query() {
-    const list = this.realm.objects(Car.schema.name);
-    console.log(list);
-  }
-}
+//   // 查询列表
+//   query() {
+//     const list = this.realm.objects(Car.schema.name);
+//     console.log(list);
+//   }
+// }
 
-export default new CarDatabase();
+// export default new CarDatabase();
