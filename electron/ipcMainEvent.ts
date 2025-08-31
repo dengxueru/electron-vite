6f759c6b606af318b975bef6d3ec2ddf13d50b9a
邓学儒 <www.dxrfx@qq.com>
2025-08-31 14:05:03 +0800

    feat: 改为 esm

--- electron/ipcMainEvent.ts
+++ electron/ipcMainEvent.ts
@@ -1,13 +1,11 @@
 import { join } from "path";
 import { app, ipcMain, IpcMainEvent } from "electron";
 import { MainArgs, MainProcessChannel } from "../types/electron";
-const clipboardFiles = require("clipboard-files");
+import clipboardFiles from "clipboard-files";
 
 import log from "electron-log";
 
 ipcMain.on(MainProcessChannel.TO_MAIN, (e: IpcMainEvent, args: MainArgs) => {
-  const binding = require("clipboard-files/build/Release/binding.node");
-  log.warn(binding.path, "tama binding.path"); // 真实文件系统路径
   log.warn(clipboardFiles);
   const downloads = join(app.getPath("downloads"), "253220000001_41180789.pdf");
   clipboardFiles.writeFiles([downloads]);
