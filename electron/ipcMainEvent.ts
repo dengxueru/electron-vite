f0bb39fde265ea3d910ba8d1c74af174d146f548
邓学儒 <dengxueru@hengli.com>
2025-10-15 14:20:48 +0800

    other: 隐藏复制

--- electron/ipcMainEvent.ts
+++ electron/ipcMainEvent.ts
@@ -1,12 +1,12 @@
 import { join } from "path";
 import { app, ipcMain, IpcMainEvent } from "electron";
 import { MainArgs, MainProcessChannel } from "../types/electron";
-import clipboardFiles from "clipboard-files";
+// import clipboardFiles from "clipboard-files";
 
 import log from "electron-log";
 
-ipcMain.on(MainProcessChannel.TO_MAIN, (e: IpcMainEvent, args: MainArgs) => {
-  log.warn(clipboardFiles);
-  const downloads = join(app.getPath("downloads"), "253220000001_41180789.pdf");
-  clipboardFiles.writeFiles([downloads]);
-});
+// ipcMain.on(MainProcessChannel.TO_MAIN, (e: IpcMainEvent, args: MainArgs) => {
+//   log.warn(clipboardFiles);
+//   const downloads = join(app.getPath("downloads"), "253220000001_41180789.pdf");
+//   clipboardFiles.writeFiles([downloads]);
+// });
